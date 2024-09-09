c1b0e0ef86778b75bf3f0bcd2b04b4226008f33c
Jakub Beránek <berykubik@gmail.com>
2024-09-09 17:05:48 +0200

    Activate the extension based on debug sessions

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -5,42 +5,65 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
 	let panel: vscode.WebviewPanel | null = null;
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand("memviz.open", () => {
-			if (panel !== null) {
-				panel.reveal();
-			} else {
-				const distDirectory = vscode.Uri.joinPath(context.extensionUri, "dist");
-				panel = vscode.window.createWebviewPanel(
-					"memviz",
-					"Memviz",
-					vscode.ViewColumn.Beside, // Show to the side of the editor.
-					{
-						localResourceRoots: [
-							distDirectory,
-							vscode.Uri.joinPath(context.extensionUri, "static"),
-						],
-						enableScripts: true
-					}
-				);
-				const htmlPath = vscode.Uri.file(path.join(context.extensionPath, "static", "index.html")).fsPath;
-				const html = new TextDecoder("UTF-8").decode(readFileSync(htmlPath));
+	const startDisposable = vscode.debug.onDidStartDebugSession(() => {
+		if (panel !== null) {
+			panel.reveal();
+		} else {
+			console.log("Opening memviz");
 
-				const scriptPath = vscode.Uri.joinPath(distDirectory, "memviz-glue.js");
-				const scriptSrc = panel.webview.asWebviewUri(scriptPath).toString();
+			panel = createPanel(context);
+			panel.onDidDispose(
+				() => {
+					panel = null;
+				},
+				null,
+				context.subscriptions
+			);
+		}
+	});
+	context.subscriptions.push(startDisposable);
 
-				panel.webview.html = html.replaceAll("$SCRIPT", scriptSrc);
-				panel.webview.postMessage({ command: "refactor" });
-				panel.onDidDispose(
-					() => {
-						panel = null;
-					},
-					null,
-					context.subscriptions
-				);
-			}
-		})
+	const endDisposable = vscode.debug.onDidTerminateDebugSession(() => {
+		console.log("Closing memviz");
+		if (panel !== null) {
+			panel.dispose();
+		}
+	});
+	context.subscriptions.push(endDisposable);
+}
+
+function createPanel(context: vscode.ExtensionContext): vscode.WebviewPanel {
+	const distDirectory = vscode.Uri.joinPath(context.extensionUri, "dist");
+	const panel = vscode.window.createWebviewPanel(
+		"memviz",
+		"Memviz",
+		vscode.ViewColumn.Beside, // Show to the side of the editor.
+		{
+			localResourceRoots: [
+				distDirectory,
+				vscode.Uri.joinPath(context.extensionUri, "static"),
+			],
+			enableScripts: true,
+			// Do not remove the state of the tab when it becomes hidden
+			retainContextWhenHidden: true
+		}
 	);
+	const htmlPath = vscode.Uri.file(path.join(context.extensionPath, "static", "index.html")).fsPath;
+	const html = new TextDecoder("UTF-8").decode(readFileSync(htmlPath));
+
+	const scriptPath = vscode.Uri.joinPath(distDirectory, "memviz-glue.js");
+	const scriptSrc = panel.webview.asWebviewUri(scriptPath).toString();
+
+	panel.webview.html = html.replaceAll("$SCRIPT", scriptSrc).replaceAll("$CSPSOURCE", panel.webview.cspSource);
+	panel.webview.onDidReceiveMessage(
+		message => {
+			console.log("Extension received message: ", message);
+		},
+		undefined,
+		context.subscriptions
+	);
+	panel.webview.postMessage({ command: "refactor" });
+	return panel;
 }
 
 export function deactivate() { }
