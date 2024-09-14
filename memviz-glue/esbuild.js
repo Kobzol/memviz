39e5f669cf2b9d770ef59409394a3368cb9e35a9
Jakub Beránek <jakub.beranek@vsb.cz>
2024-09-14 12:30:16 +0200

    Add Biome formatter and linter

--- memviz-glue/esbuild.js
+++ memviz-glue/esbuild.js
@@ -8,61 +8,60 @@ const watch = process.argv.includes("--watch");
  * @type {import('esbuild').Plugin}
  */
 const esbuildProblemMatcherPlugin = {
-	name: 'esbuild-problem-matcher',
+  name: "esbuild-problem-matcher",
 
-	setup(build) {
-		build.onStart(() => {
-			console.log('[watch] build started');
-		});
-		build.onEnd((result) => {
-			result.errors.forEach(({ text, location }) => {
-				console.error(`✘ [ERROR] ${text}`);
-				console.error(`    ${location.file}:${location.line}:${location.column}:`);
-			});
-			console.log('[watch] build finished');
-		});
-	},
+  setup(build) {
+    build.onStart(() => {
+      console.log("[watch] build started");
+    });
+    build.onEnd((result) => {
+      result.errors.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`);
+        console.error(
+          `    ${location.file}:${location.line}:${location.column}:`,
+        );
+      });
+      console.log("[watch] build finished");
+    });
+  },
 };
 
 const OUTPUT_PATH = "dist/index.js";
 
 const copyFileToExtension = {
-	name: "copy-file-to-extension",
-	setup(build) {
-		build.onEnd((result) => {
-			if (result.errors.length === 0) {
-				console.log("Copying output file to extension");
-				fs.copyFileSync(OUTPUT_PATH, "../extension/dist/memviz-glue.js");
-			}
-		});
-	}
+  name: "copy-file-to-extension",
+  setup(build) {
+    build.onEnd((result) => {
+      if (result.errors.length === 0) {
+        console.log("Copying output file to extension");
+        fs.copyFileSync(OUTPUT_PATH, "../extension/dist/memviz-glue.js");
+      }
+    });
+  },
 };
 
 async function main() {
-	const ctx = await esbuild.context({
-		entryPoints: ["src/index.ts"],
-		bundle: true,
-		minify: production,
-		sourcemap: !production,
-		sourcesContent: false,
-		platform: "browser",
-		outfile: OUTPUT_PATH,
-		logLevel: "silent",
-		plugins: [
-			esbuildProblemMatcherPlugin,
-			copyFileToExtension
-		],
-	});
+  const ctx = await esbuild.context({
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    platform: "browser",
+    outfile: OUTPUT_PATH,
+    logLevel: "silent",
+    plugins: [esbuildProblemMatcherPlugin, copyFileToExtension],
+  });
 
-	if (watch) {
-		await ctx.watch();
-	} else {
-		await ctx.rebuild();
-		await ctx.dispose();
-	}
+  if (watch) {
+    await ctx.watch();
+  } else {
+    await ctx.rebuild();
+    await ctx.dispose();
+  }
 }
 
-main().catch(e => {
-	console.error(e);
-	process.exit(1);
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
 });
