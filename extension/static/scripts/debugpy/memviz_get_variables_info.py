bbaf5e22c8d144f7d4cd6019f09740edd9f4678b
Sofia Michailidu <sofia@michailidu.cz>
2025-10-20 14:36:24 +0200

    add python variable representation

--- extension/static/scripts/debugpy/memviz_get_variables_info.py
+++ extension/static/scripts/debugpy/memviz_get_variables_info.py
@@ -1,18 +1,134 @@
+from abc import ABC
 import dataclasses
 import json
-from typing import Any, Callable, List, Optional, Tuple
+from typing import Any, Callable, Dict, List, Optional
+
+
+PythonId = str
+
+
+@dataclasses.dataclass(frozen=True)
+class Place:
+    name: str
+    id: PythonId
+
+
+@dataclasses.dataclass(frozen=True)
+class BaseVal(ABC):
+    size: int
+
+
+@dataclasses.dataclass(frozen=True)
+class DeferredVal(BaseVal, ABC):
+    reference: str
+
+
+@dataclasses.dataclass(frozen=True)
+class NoneVal(BaseVal):
+    kind: str = dataclasses.field(init=False, default="none")
+
+
+@dataclasses.dataclass(frozen=True)
+class BoolVal(BaseVal):
+    kind: str = dataclasses.field(init=False, default="bool")
+    value: bool
+
+
+@dataclasses.dataclass(frozen=True)
+class IntVal(BaseVal):
+    kind: str = dataclasses.field(init=False, default="int")
+    value: str
+
+
+@dataclasses.dataclass(frozen=True)
+class FloatVal(BaseVal):
+    kind: str = dataclasses.field(init=False, default="float")
+    value: str
+
+
+@dataclasses.dataclass(frozen=True)
+class ComplexVal(BaseVal):
+    kind: str = dataclasses.field(init=False, default="complex")
+    real_value: str
+    imaginary_value: str
+
+
+@dataclasses.dataclass(frozen=True)
+class DeferredStrVal(DeferredVal):
+    kind: str = dataclasses.field(init=False, default="defStr")
+    length: int
+
+
+@dataclasses.dataclass(frozen=True)
+class DeferredListVal(DeferredVal):
+    kind: str = dataclasses.field(init=False, default="defList")
+    element_count: int
+
+
+@dataclasses.dataclass(frozen=True)
+class DeferredTupleVal(DeferredVal):
+    kind: str = dataclasses.field(init=False, default="defTuple")
+    element_count: int
+
+
+@dataclasses.dataclass(frozen=True)
+class DeferredSetVal(DeferredVal):
+    kind: str = dataclasses.field(init=False, default="defSet")
+    element_count: int
+
+
+@dataclasses.dataclass(frozen=True)
+class DeferredFrozenSetVal(DeferredVal):
+    kind: str = dataclasses.field(init=False, default="defFrozenSet")
+    element_count: int
+
+
+@dataclasses.dataclass(frozen=True)
+class DeferredDictVal(DeferredVal):
+    kind: str = dataclasses.field(init=False, default="defDict")
+    key_value_pair_count: int
+
+
+@dataclasses.dataclass(frozen=True)
+class KeyValuePair:
+    key: BaseVal
+    value: BaseVal
+
+
+@dataclasses.dataclass(frozen=True)
+class RangeVal(BaseVal):
+    kind: str = dataclasses.field(init=False, default="range")
+    start: str
+    stop: str
+    step: str
+
+
+@dataclasses.dataclass(frozen=True)
+class FunctionVal(BaseVal):
+    kind: str = dataclasses.field(init=False, default="function")
+    name: str
+    qualified_name: str
+    module: str | None
+    signature: str | None
+
+
+@dataclasses.dataclass(frozen=True)
+class DeferredObjectVal(DeferredVal):
+    kind: str = dataclasses.field(init=False, default="defObject")
+    type_name: str
+
+
+@dataclasses.dataclass(frozen=True)
+class ObjectVal(BaseVal):
+    kind: str = dataclasses.field(init=False, default="object")
+    attributes: List[KeyValuePair] = dataclasses.field(default_factory=list)
+    methods: List[str] = dataclasses.field(default_factory=list)
 
 
 @dataclasses.dataclass
-class VariableInformation:
-    name: str
-    type: str
-    value: str
-    id: str
-
-
-def get_frame_places(frame_index: int) -> List[VariableInformation]:
-    return "test"
+class Variables:
+    places: List[Place]
+    values: Dict[PythonId, BaseVal]
 
 
 @dataclasses.dataclass
@@ -36,6 +152,7 @@ def try_run(fn: Callable) -> str:
         return dataclass_to_json(Result.make_ok(result))
     except BaseException as e:
         return dataclass_to_json(Result.make_error(str(e)))
-    
+
+
 def dataclass_to_json(value) -> str:
     return json.dumps(dataclasses.asdict(value))
