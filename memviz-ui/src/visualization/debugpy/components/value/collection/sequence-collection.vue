a701fd734d02f5c6021e63cd7d012e3b0de9e427
Sofia Michailidu <sofia@michailidu.cz>
2025-12-17 22:24:59 +0100

    add sequence navigation controls

--- memviz-ui/src/visualization/debugpy/components/value/collection/sequence-collection.vue
+++ memviz-ui/src/visualization/debugpy/components/value/collection/sequence-collection.vue
@@ -1,19 +1,87 @@
 <script setup lang="ts">
-import { DeferredListVal, DeferredTupleVal } from "process-def/debugpy";
+import { ref, computed } from "vue";
+import { DeferredListVal, DeferredTupleVal, Value } from "process-def/debugpy";
 import MemorySlot from "../../memory-slot.vue";
 
 const props = defineProps<{
   value: DeferredListVal | DeferredTupleVal;
 }>();
+
+const handleIndexInput = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  let newIndex = parseInt(target.value, 10);
+  if (isNaN(newIndex)) return;
+  if (newIndex < 0) newIndex = 0;
+  if (newIndex >= totalElements.value) newIndex = totalElements.value - 1;
+  currentIndex.value = newIndex;
+};
+const currentIndex = ref(0);
+const visibleElementCount = 5;
+
+const totalElements = computed(() => props.value.element_count);
+const canGoToPrevious = computed(() => currentIndex.value > 0);
+const canGoToNext = computed(
+  () => currentIndex.value + visibleElementCount < totalElements.value
+);
+
+const goToPrevious = () => {
+  if (canGoToPrevious.value) currentIndex.value--;
+};
+
+const goToNext = () => {
+  if (canGoToNext.value) currentIndex.value++;
+};
+
+const visibleElements = computed(() => {
+  const elements: { [key: number]: Value } = {};
+  const lastIndex = Math.min(
+    currentIndex.value + visibleElementCount,
+    totalElements.value
+  );
+  for (let i = currentIndex.value; i < lastIndex; i++) {
+    elements[i] = props.value.elements[i];
+  }
+  return elements;
+});
 </script>
 
 <template>
   <div class="sequence">
     <table class="elements">
-      <tr v-for="(el, index) in value.elements" :key="index">
+      <tr class="control-row">
+        <td colspan="2">
+          <div class="control-wrapper">
+            <button
+              class="nav-btn"
+              @click="goToPrevious"
+              :disabled="!canGoToPrevious"
+            >
+              &#9650
+            </button>
+            <input
+              class="index-input"
+              type="number"
+              :value="currentIndex"
+              @input="handleIndexInput"
+              :min="0"
+              :max="totalElements - 1"
+            />
+          </div>
+        </td>
+      </tr>
+
+      <tr v-for="(el, index) in visibleElements" :key="index">
         <td class="index">{{ index }}</td>
         <td class="value">
           <MemorySlot :value="el" />
+        </td>
+      </tr>
+
+      <tr class="control-row">
+        <td colspan="2">
+          <button class="nav-btn" @click="goToNext" :disabled="!canGoToNext">
+            &#9660
+          </button>
         </td>
       </tr>
     </table>
@@ -34,6 +102,7 @@ td {
   border: 3px solid black;
 
   margin: 5px 5px 0 0;
+  width: 100%;
 
   .index {
     background-color: #dae4ef;
@@ -44,7 +113,44 @@ td {
   }
 }
 
+.control-row td {
+  padding: 0;
+  background: #f4f4f4;
+}
+
+.control-wrapper {
+  display: flex;
+  align-items: stretch;
+  width: 100%;
+}
+
+.nav-btn {
+  flex-grow: 1;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  font-size: 1.1em;
+  width: 100%;
+
+  &:disabled {
+    color: #ccc;
+    cursor: not-allowed;
+  }
+
+  &:hover:not(:disabled) {
+    background-color: #e2e2e2;
+  }
+}
+
+.index-input {
+  width: 60px;
+  border: none;
+  border-left: 1px solid #858585;
+  text-align: center;
+  background: white;
+}
+
 .value {
-  margin-left: 5px;
+  padding-left: 5px;
 }
 </style>
